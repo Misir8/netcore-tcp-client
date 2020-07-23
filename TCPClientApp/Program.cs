1bb0b40c32ecf653bc7fc23c8705a86fbb550973
Misir <misirua@code.edu.az>
2020-07-23 14:06:48 +0400

    Initial Create

---
+++ TCPClientApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace TCPClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hostname: ");
+            string host = Console.ReadLine();
+            Console.WriteLine("Port: ");
+            int port;
+            string portInput = Console.ReadLine();
+            int.TryParse(portInput, out port);
+           
+
+            try
+            {
+                
+                TcpClient tcpClient = new TcpClient();
+                tcpClient.Connect(host, port);
+                Console.WriteLine("The connection was successful");
+                tcpClient.Close();
+                Console.WriteLine("The end connection");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine($"Exception", e.Message);
+            }
+        }
+    }
+}
